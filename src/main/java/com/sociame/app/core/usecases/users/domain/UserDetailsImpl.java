dda69410ee83e841e3bfa50e94dd43f80162b46e
Tasos Daris <tasos.daris@datawise.ai>
2023-10-25 15:46:00 +0300

    cleanup: Remove unused import.
    Refs: #
    Committed-by: Tasos Daris <tasos.daris@datawise.com>
    Signed-off-by: Tasos Daris <tasos.daris@datawise.ai>

--- src/main/java/com/sociame/app/core/usecases/users/domain/UserDetailsImpl.java
+++ src/main/java/com/sociame/app/core/usecases/users/domain/UserDetailsImpl.java
@@ -1,6 +1,5 @@
 package com.sociame.app.core.usecases.users.domain;
 
-import jakarta.validation.OverridesAttribute;
 import lombok.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
