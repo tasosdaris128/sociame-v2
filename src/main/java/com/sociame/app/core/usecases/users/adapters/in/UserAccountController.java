0e222f77ecd5ce9369dd7114ca2a46f06872cabc
Tasos Daris <tasos.daris@datawise.ai>
2023-10-26 08:50:03 +0300

    doc: Mark UserAccountController for removal.
    Refs: #
    Committed-by: Tasos Daris <tasos.daris@datawise.com>
    Signed-off-by: Tasos Daris <tasos.daris@datawise.ai>

--- src/main/java/com/sociame/app/core/usecases/users/adapters/in/UserAccountController.java
+++ src/main/java/com/sociame/app/core/usecases/users/adapters/in/UserAccountController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 
+@Deprecated(forRemoval = true)
 @Slf4j
 @RestController
 @RequiredArgsConstructor
