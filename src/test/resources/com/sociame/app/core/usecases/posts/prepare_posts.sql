b4a7e1361f4f07cb357262105c4bf69de0fd6941
Tasos Daris <tasos.daris@datawise.ai>
2023-11-03 18:55:56 +0200

    test: Prepare post test cases DB.
    Refs: #
    Committed-by: Tasos Daris <tasos.daris@datawise.com>
    Signed-off-by: Tasos Daris <tasos.daris@datawise.ai>

--- src/test/resources/com/sociame/app/core/usecases/posts/prepare_posts.sql
+++ src/test/resources/com/sociame/app/core/usecases/posts/prepare_posts.sql
@@ -1,2 +1,89 @@
-insert into public.users(username, password, authorities, created_at, enabled, expired, locked, credentials_expired)
-VALUES ('manos', '$2a$10$fsD9VR/Zn0voWu.zgfRaLOopJaYXkwhaVm90SfxfGuaGsVRR7LAbq', array['authority_1', 'role_admin'], '2023-01-01 11:59:59.000', true, false, false, false);
+INSERT INTO public.users(
+    username,
+    password,
+    authorities,
+    created_at,
+    enabled,
+    expired,
+    locked,
+    credentials_expired
+)
+VALUES (
+    'freeplan@example.com',
+    '$2a$10$fsD9VR/Zn0voWu.zgfRaLOopJaYXkwhaVm90SfxfGuaGsVRR7LAbq',
+    array[''],
+    '2023-01-01 11:59:59.000',
+    true,
+    false,
+    false,
+    false
+);
+
+INSERT INTO public.account(
+    first_name,
+    last_name,
+    gender,
+    plan,
+    user_id,
+    verified
+)
+VALUES (
+    'Free',
+    'Plan',
+    'M',
+    1,
+    1,
+    true
+);
+
+INSERT INTO public.users(
+    username,
+    password,
+    authorities,
+    created_at,
+    enabled,
+    expired,
+    locked,
+    credentials_expired
+)
+VALUES (
+    'premiumplan@example.com',
+    '$2a$10$fsD9VR/Zn0voWu.zgfRaLOopJaYXkwhaVm90SfxfGuaGsVRR7LAbq',
+    array[''],
+    '2023-01-01 11:59:59.000',
+    true,
+    false,
+    false,
+    false
+);
+
+INSERT INTO public.account(
+    first_name,
+    last_name,
+    gender,
+    plan,
+    user_id,
+    verified
+)
+VALUES (
+    'Premium',
+    'Plan',
+    'M',
+    2,
+    2,
+    true
+);
+
+INSERT INTO public.post(title, body, author_id) VALUES('A post', '...and its body.', 1);
+
+INSERT INTO public.comment(body, post_id, author_id) VALUES('Free Comment 0', 1, 1);
+INSERT INTO public.comment(body, post_id, author_id) VALUES('Free Comment 1', 1, 1);
+INSERT INTO public.comment(body, post_id, author_id) VALUES('Free Comment 2', 1, 1);
+INSERT INTO public.comment(body, post_id, author_id) VALUES('Free Comment 3', 1, 1);
+INSERT INTO public.comment(body, post_id, author_id) VALUES('Free Comment 4', 1, 1);
+
+INSERT INTO public.comment(body, post_id, author_id) VALUES('Premium Comment 0', 1, 2);
+INSERT INTO public.comment(body, post_id, author_id) VALUES('Premium Comment 1', 1, 2);
+INSERT INTO public.comment(body, post_id, author_id) VALUES('Premium Comment 2', 1, 2);
+INSERT INTO public.comment(body, post_id, author_id) VALUES('Premium Comment 3', 1, 2);
+INSERT INTO public.comment(body, post_id, author_id) VALUES('Premium Comment 4', 1, 2);
